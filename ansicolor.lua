7e2726166bc0de0660be261e382136f06e73a2f4
Jacob Gardner <operator-DD3@users.noreply.github.com>
2015-08-19 09:41:21 -0500

    Create ansicolor.lua

---
+++ ansicolor.lua
@@ -0,0 +1,26 @@
+color = {}
+color.black = "\27[30m"
+color.red = "\27[31m"
+color.green = "\27[32m"
+color.yellow = "\27[33m"
+color.blue = "\27[34m"
+color.purple = "\27[35m"
+color.cyan = "\27[36m"
+color.white = "\27[37m"
+color.bgblack = "\27[40m"
+color.bgred = "\27[41m"
+color.bggreen = "\27[42m"
+color.bgyellow = "\27[43m"
+color.bgblue = "\27[44m"
+color.bgpurple = "\27[45m"
+color.bgcyan = "\27[46m"
+color.bgwhite = "\27[47m"
+color.bold = "\27[1m"
+color.italics = "\27[3m"
+color.underline = "\27[4m"
+color.strike = "\27[9m"
+color.reset = "\27[0m"
+color.gray = color.cyan
+color.grey = color.cyan
+
+--io.write(color.red,"Hello",color.blue ,", ",color.bgyellow, "World!",color.reset,"!!")
